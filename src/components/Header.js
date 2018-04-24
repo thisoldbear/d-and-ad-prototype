8e7131797b37d4b9efc769f3e9b65a5b2517bc04
Daniel Williams <dwilliams@zonedigital.com>
2018-04-24 17:16:07 +0100

    add header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+
+import './Header.css';
+
+import logo from './../logo.svg';
 
 // The Header creates links that can be used to navigate
 // between routes.
 const Header = () => (
-  <header>
-    <nav>
-      <ul>
-        <li><Link to='/'>Homepage</Link></li>
-        <li><Link to='/case-study'>Case Study</Link></li>
-        <li><Link to='/profile'>Profile</Link></li>
-        <li><Link to='/nomination'>Nomination</Link></li>
+  <header className="site-header">
+    <img src={logo} className="site-header__logo" alt="logo" />
+    <nav className="site-header__nav">
+      <ul className="site-header__nav-list">
+        <li className="site-header__nav-list-item"><Link className="site-header__nav-list-link" to='/'>Homepage</Link></li>
+        <li className="site-header__nav-list-item"><Link className="site-header__nav-list-link" to='/case-study'>Case Study</Link></li>
+        <li className="site-header__nav-list-item"><Link className="site-header__nav-list-link" to='/profile'>Profile</Link></li>
+        <li className="site-header__nav-list-item"><Link className="site-header__nav-list-link" to='/nomination'>Nomination</Link></li>
       </ul>
     </nav>
   </header>
