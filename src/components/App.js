09748a3d442e3df276bf8858ed1c434842faad7a
Daniel Williams <dwilliams@zonedigital.com>
2018-04-24 12:26:56 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import Header from './Header';
+import Main from './Main';
+
+import './App.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CaseStudy.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CaseStudy = () => (
+  <div>
+    <h1>Case Study</h1>
+  </div>
+)
+
+export default CaseStudy
